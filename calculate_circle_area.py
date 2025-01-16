e04af3c1056dfefa8a048ce3fe4b70c3edcacb8b
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2025-01-16 14:51:28 +0400

    lesson 1: create function calculate_circle_area & Rectangle class

--- calculate_circle_area.py
+++ calculate_circle_area.py
@@ -1,1 +1,8 @@
+import math
 
+def calculate_circle_area(radius):
+    return (radius ** 2) * math.pi
+
+data = int(input("Enter radius of circle: "))
+area = calculate_circle_area(data)
+print(area)
--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,32 @@
 
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width + self.height) * 2
+
+    def is_square(self):
+        return self.width == self.height
+
+    def resize(self, new_width, new_height):
+        self.width = new_width
+        self.height = new_height
+
+
+rectangle = Rectangle(5, 10)
+
+print("Rectangle area is: ", rectangle.area())
+print("Rectangle perimeter is: ", rectangle.perimeter())
+print("Rectangle is square: ", rectangle.is_square())
+
+rectangle.resize(6, 6)
+
+print("Rectangle area is: ", rectangle.area())
+print("Rectangle perimeter is: ", rectangle.perimeter())
+print("Rectangle is square: ", rectangle.is_square())
